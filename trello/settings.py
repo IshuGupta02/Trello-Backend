502e93813ec6b712751921390be8a5815b1358b6
IshuGupta02 <ishugupta0298@gmail.com>
2021-10-03 09:07:34 +0530

    PUSH request errors resolved

--- trello/settings.py
+++ trello/settings.py
@@ -161,16 +161,24 @@ REST_FRAMEWORK = {
 
 
 SESSION_ENGINE = "django.contrib.sessions.backends.cached_db"
-CORS_ALLOW_CREDENTIALS = True
+# CORS_ALLOW_CREDENTIALS = True
 
 # CORS_ORIGIN_WHITELIST = [
 #     'http://localhost:3000',
 #     'http://127.0.0.1:3000'
 # ]
 
+CORS_ORIGIN_WHITELIST = (
+         'http://127.0.0.1:8000',
+         'http://127.0.0.1:3000',
+     )
+
+CORS_ALLOW_CREDENTIALS=True
+CORS_ORIGIN_ALLOW_ALL = False
 
 
-CORS_ORIGIN_ALLOW_ALL = True
+
+# CORS_ORIGIN_ALLOW_ALL = True
 
 # CORS_ALLOWED_ORIGINS = [
 #     'http://localhost:3000',
