b0257e75b4c5dd92efe723805006b543bc48eaf4
rochakjain361 <rochak_r@ec.iitr.ac.in>
2021-10-07 16:17:39 +0530

    Refractor views.py using PEP8 conventions

--- api/views.py
+++ api/views.py
@@ -2,42 +2,28 @@
 Contains all views
 """
 from rest_framework.decorators import api_view
-from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.http import JsonResponse, HttpResponseForbidden, HttpResponseBadRequest
-import json
 import requests
 from rest_framework.response import Response
 from .login_config import config
 from . import models
 from django.contrib.auth import login, logout
 from rest_framework import viewsets
-from .serializers import UserSerializer,ProjectSerializer,ListSerializer,CardSerializer,CommentSerializer, ProjectSerializer1, CardSerializer1
+from .serializers import UserSerializer, ProjectSerializer, ListSerializer, CardSerializer, CommentSerializer, \
+    ProjectSerializer1, CardSerializer1
 from rest_framework.decorators import action
-from rest_framework.views import APIView
-from .permissions import IsUserEnabled, IsAdminOrProjectAdminOrReadOnly, IsAdmin, IsOwnerOrReadOnly, IsTeamMemberOrReadOnly_List, IsTeamMemberOrReadOnly_Project, IsTeamMemberOrReadOnly_Card, Not_allowed
+from .permissions import IsUserEnabled, IsAdminOrProjectAdminOrReadOnly, IsAdmin, IsOwnerOrReadOnly, \
+    IsTeamMemberOrReadOnly_List, IsTeamMemberOrReadOnly_Card, Not_allowed
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework import status
+
 
 @api_view(['GET'])
 def check(request):
     """
     checks whether or not a user is logged in
     """
-    # print("print1")
-    # if(request.user.is_authenticated):
-    #     print("print2")
-    #     res= Response({'status': 'loggedIn'}, status=status.HTTP_202_ACCEPTED)
-    # else:
-    #     print("print3")
-    #     res= Response({'status': 'NotloggedIn'}, status=status.HTTP_202_ACCEPTED)
-
-    # print("print4")
-    # res['Access-Control-Allow-Origin']='http://127.0.0.1:3000'
-    # res['Access-Control-Allow-Credentials']='true'
-    # print("print5")
-    # return res
-
     msg = {
         "loggedin": False
     }
@@ -53,6 +39,7 @@ def check(request):
         res['Access-Control-Allow-Credentials'] = 'true'
         return res
 
+
 class LoginViewSet(viewsets.ModelViewSet):
     """
     A viewset that provides the standard actions like login, logout
@@ -64,7 +51,7 @@ class LoginViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
 
     @action(detail=False, url_path='login', url_name='login-login')
-    def login1(self, req):
+    def login1(self):
         """
         It will redirect to oauth page
         from /api/login--> oauth
@@ -80,14 +67,9 @@ class LoginViewSet(viewsets.ModelViewSet):
         """
         try:
             auth_code = self.request.query_params.get('code')
-            print(auth_code)
-            # auth_code = self.request.query_params.get('code')
-
         except:
-            print("bad1")
             return HttpResponseBadRequest()
 
-        # print("print1")
         params = {
             'client_id': config['CLIENT_ID'],
             'client_secret': config['CLIENT_SECRET'],
@@ -96,89 +78,64 @@ class LoginViewSet(viewsets.ModelViewSet):
             'code': auth_code,
         }
 
-        # print("print2")
         res = requests.post("https://channeli.in/open_auth/token/", data=params)
-
-        # print("print3")
-
-        # print("print3_next_before")
 
         if (res.status_code == 200):
             access_token = res.json()['access_token']
             refresh_token = res.json()['refresh_token']
         else:
-            print("bad")
             return HttpResponseBadRequest()
 
-        # print("print3_next")
         header = {
             "Authorization": "Bearer " + access_token,
         }
-
-        # print("print4")
 
         res1 = requests.get("https://channeli.in/open_auth/get_user_data/", headers=header)
         data_final = res1.json()
 
         isMaintainer = False
-        active=True
+        active = True
         for role in data_final['person']['roles']:
-            print(role['role'])
-            if (role['role'] == 'Maintainer'):
+            if role['role'] == 'Maintainer':
                 isMaintainer = True
-                if(role['activeStatus']!='ActiveStatus.IS_ACTIVE'):
-                    active=False
+                if role['activeStatus'] != 'ActiveStatus.IS_ACTIVE':
+                    active = False
 
         if not active:
             try:
                 models.User.objects.filter(enrollment_no=data_final['username']).delete()
             except:
-                error=True
+                error = True
             return JsonResponse({'status': 'you are not active anymore'})
 
         if not isMaintainer:
             return JsonResponse({'status': 'you are not a maintainer'})
-
-        print(data_final)
 
         try:
             user = models.User.objects.get(enrollment_no=data_final['username'])
 
         except:
             user_name = data_final['person']['fullName']
-            email_id=data_final['contactInformation']['emailAddress']
+            email_id = data_final['contactInformation']['emailAddress']
             ern = data_final['username']
             isAdmin = False
             isEnabled = True
-            profile_url=data_final['person']['displayPicture']
-            user = models.User(enrollment_no=ern, User_name=user_name, admin=isAdmin, enabled=isEnabled, email=email_id, profile=profile_url)
+            profile_url = data_final['person']['displayPicture']
+            user = models.User(enrollment_no=ern, User_name=user_name, admin=isAdmin, enabled=isEnabled, email=email_id,
+                               profile=profile_url)
             print("saving")
             user.save()
             print("saved")
         login(request=req, user=user)
-
-        print(req.auth)
-        print(req.user)
-        print(req.user.is_authenticated)
-
-        info={
-            'data':'Done!', 
-            'isAdmin':user.admin , 
-            'isEnabled' : user.enabled
+        info = {
+            'data': 'Done!',
+            'isAdmin': user.admin,
+            'isEnabled': user.enabled
         }
-
-        res= Response(info, status=status.HTTP_202_ACCEPTED)
-        res['Access-Control-Allow-Origin']='http://127.0.0.1:3000'
-        res['Access-Control-Allow-Credentials']='true'
-        # res['withCredentials']='true'
-
-        # res['Access-Control-Expose-Headers']='*'
-
-        # access-control-expose-headers: Set-Cookie
+        res = Response(info, status=status.HTTP_202_ACCEPTED)
+        res['Access-Control-Allow-Origin'] = 'http://127.0.0.1:3000'
+        res['Access-Control-Allow-Credentials'] = 'true'
         return res
-
-        # return Response({'data':'Done!', 'isAdmin':user.admin , 'isEnabled' : user.enabled},status=status.HTTP_202_ACCEPTED)
-        # return HttpResponse("done!")
 
     @action(methods=['GET'], detail=False, url_path='logout', url_name='login-logout')
     def logout_(self, request):
@@ -187,13 +144,14 @@ class LoginViewSet(viewsets.ModelViewSet):
         """
         if request.user.is_authenticated:
             logout(request)
-            res= Response({'status': 'successful'}, status=status.HTTP_202_ACCEPTED)
-            res['Access-Control-Allow-Origin']='http://127.0.0.1:3000'
-            res['Access-Control-Allow-Credentials']='true'
+            res = Response({'status': 'successful'}, status=status.HTTP_202_ACCEPTED)
+            res['Access-Control-Allow-Origin'] = 'http://127.0.0.1:3000'
+            res['Access-Control-Allow-Credentials'] = 'true'
             # return JsonResponse({'status': 'successful'})
             return res
         else:
             return HttpResponseForbidden()
+
 
 class UserViewSet(viewsets.ModelViewSet):
     """
@@ -204,57 +162,35 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = models.User.objects.all()
     serializer_class = UserSerializer
 
-    # @action(methods= ['GET'], detail=False, url_path='myprojects', url_name='user-myprojects')
-    # def projects(self, request):
-    #     print(request.user)
-    #     projects_data=ProjectSerializer(request.user.member.all(), many=True)
-    #     return Response(projects_data.data)
+    def __init__(self):
+        self.permission_classes = [IsAuthenticated, IsUserEnabled, IsAdmin]
 
-    # @action(methods= ['GET'],detail=False, url_path='mycards', url_name='user-mycards')
-    # def cards(self, request):
-    #     cards_data=CardSerializer(request.user.mycards.all(), many=True)
-    #     return Response(cards_data.data)
     def dispatch(self, *args, **kwargs):
         response = super(UserViewSet, self).dispatch(*args, **kwargs)
-        response['Access-Control-Allow-Origin']='http://127.0.0.1:3000'
-        response['Access-Control-Allow-Credentials']='true'
+        response['Access-Control-Allow-Origin'] = 'http://127.0.0.1:3000'
+        response['Access-Control-Allow-Credentials'] = 'true'
         return response
 
     def get_permissions(self):
         """
-        everbody can get details of users but only admins can enable/disable, change to admin/remove from admin
+        everybody can get details of users but only admins can enable/disable, change to admin/remove from admin
         """
         if self.request.method == 'GET':
             self.permission_classes = [IsUserEnabled, IsAuthenticated]
         elif self.request.method == 'POST' or self.request.method == 'DELETE':
             self.permission_classes = [Not_allowed]
         else:
-            self.permission_classes = [IsAuthenticated,IsUserEnabled, IsAdmin]
+            pass
         return super(UserViewSet, self).get_permissions()
 
     @action(methods=['GET'], detail=False, url_path='info', url_name='user-info')
     def info(self, request):
-        # print(request.user)
-        # # user_data=models.User.objects.get(request.user)
-        # return Response("user")
-
-        # print("1")
-
         info = UserSerializer(request.user)
-        # return Response(info.data)
-
-        # print("2")
-
-        res= Response(info.data, status=status.HTTP_202_ACCEPTED)
-        res['Access-Control-Allow-Origin']='http://127.0.0.1:3000'
-        res['Access-Control-Allow-Credentials']='true'
-
-        # print("3")
-
+        res = Response(info.data, status=status.HTTP_202_ACCEPTED)
+        res['Access-Control-Allow-Origin'] = 'http://127.0.0.1:3000'
+        res['Access-Control-Allow-Credentials'] = 'true'
         return res
 
-        # {headers:{"Access-Control-Request-Headers": "x-requested-with", mode: "cors"}}
-        # , headers={"Access-Control-Allow-Headers": "*"}
 
 class ProjectViewSet(viewsets.ModelViewSet):
     """
@@ -266,25 +202,13 @@ class ProjectViewSet(viewsets.ModelViewSet):
     queryset = models.Project.objects.all()
     serializer_class = ProjectSerializer
 
-    # @action(methods=['GET'], detail=False, url_path='list', url_name='project-list')
-    # def list_project(self, request):
-    #     proj= models.Project.objects.all()
-    #     proj_data=ProjectSerializer(proj, many=True)
-    #     return Response(proj_data.data)
-    #
-    # @action(methods=['POST'], detail=False, url_path='create', url_name='project-create')
-    # def create_project(self, request):
-    #     proj_data=ProjectSerializer(proj, many=True)
-    #     return Response(proj_data.data)
-
-    # def perform_create(self, serializer):
-    #     serializer.save()
+    def __init__(self):
+        self.permission_classes = [IsAuthenticated, IsUserEnabled, IsAdminOrProjectAdminOrReadOnly]
 
     def dispatch(self, *args, **kwargs):
         response = super(ProjectViewSet, self).dispatch(*args, **kwargs)
-        response['Access-Control-Allow-Origin']='http://127.0.0.1:3000'
-        response['Access-Control-Allow-Credentials']='true'
-
+        response['Access-Control-Allow-Origin'] = 'http://127.0.0.1:3000'
+        response['Access-Control-Allow-Credentials'] = 'true'
         return response
 
     def get_permissions(self):
@@ -295,8 +219,9 @@ class ProjectViewSet(viewsets.ModelViewSet):
         if self.request.method == 'GET' or self.request.method == 'POST':
             self.permission_classes = [IsUserEnabled, IsAuthenticated]
         else:
-            self.permission_classes = [IsAuthenticated,IsUserEnabled, IsAdminOrProjectAdminOrReadOnly]
+            pass
         return super(ProjectViewSet, self).get_permissions()
+
 
 class ListViewSet(viewsets.ModelViewSet):
     """
@@ -308,6 +233,9 @@ class ListViewSet(viewsets.ModelViewSet):
     queryset = models.List.objects.all()
     serializer_class = ListSerializer
 
+    def __init__(self):
+        self.permission_classes = [IsAuthenticated, IsUserEnabled, IsTeamMemberOrReadOnly_List]
+
     def get_permissions(self):
         """
         anyone can get information about any list
@@ -316,9 +244,10 @@ class ListViewSet(viewsets.ModelViewSet):
         if self.request.method == 'GET':
             self.permission_classes = [IsUserEnabled, IsAuthenticated]
         else:
-            self.permission_classes = [IsAuthenticated,IsUserEnabled, IsTeamMemberOrReadOnly_List]
+            pass
 
         return super(ListViewSet, self).get_permissions()
+
 
 class CardViewSet(viewsets.ModelViewSet):
     """
@@ -331,6 +260,9 @@ class CardViewSet(viewsets.ModelViewSet):
     queryset = models.Card.objects.all()
     serializer_class = CardSerializer
 
+    def __init__(self):
+        self.permission_classes = [IsAuthenticated, IsUserEnabled, IsTeamMemberOrReadOnly_Card]
+
     def get_permissions(self):
         """
         anyone can get information about any card
@@ -340,9 +272,10 @@ class CardViewSet(viewsets.ModelViewSet):
         if self.request.method == 'GET':
             self.permission_classes = [IsUserEnabled, IsAuthenticated]
         else:
-            self.permission_classes = [IsAuthenticated,IsUserEnabled, IsTeamMemberOrReadOnly_Card]
+            pass
 
         return super(CardViewSet, self).get_permissions()
+
 
 class CommentViewSet(viewsets.ModelViewSet):
     """
@@ -354,6 +287,9 @@ class CommentViewSet(viewsets.ModelViewSet):
     queryset = models.Comment.objects.all()
     serializer_class = CommentSerializer
 
+    def __init__(self):
+        self.permission_classes = [IsAuthenticated, IsUserEnabled, IsOwnerOrReadOnly]
+
     def get_permissions(self):
         """
         everyone can view comments/ create a new comment
@@ -362,7 +298,7 @@ class CommentViewSet(viewsets.ModelViewSet):
         if self.request.method == 'GET' or self.request.method == 'POST':
             self.permission_classes = [IsUserEnabled, IsAuthenticated]
         else:
-            self.permission_classes = [IsAuthenticated,IsUserEnabled, IsOwnerOrReadOnly]
+            pass
         return super(CommentViewSet, self).get_permissions()
 
 
@@ -378,10 +314,10 @@ class ProjectDataViewSet(viewsets.ModelViewSet):
 
     def dispatch(self, *args, **kwargs):
         response = super(ProjectDataViewSet, self).dispatch(*args, **kwargs)
-        response['Access-Control-Allow-Origin']='http://127.0.0.1:3000'
-        response['Access-Control-Allow-Credentials']='true'
-
+        response['Access-Control-Allow-Origin'] = 'http://127.0.0.1:3000'
+        response['Access-Control-Allow-Credentials'] = 'true'
         return response
+
 
 class CardDataViewSet(viewsets.ModelViewSet):
     """
@@ -390,9 +326,5 @@ class CardDataViewSet(viewsets.ModelViewSet):
     3. update a card- card_name, assigned_to,description
     4. delete a card
     """
-
     queryset = models.Card.objects.all()
     serializer_class = CardSerializer1
-
-
-   
